41222f56dc55c4c86570ff0a7acfd1bf65395a64
Andres Camilo Chavez <acchavezr@rtisas.com.co>
2024-09-24 20:56:26 -0500

    Código para que controlar la carga de un componente es decir como un if y else también está la opción on viewport que permite cargarlo cuando se vea

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component copy';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-defer-views',
   standalone: true,
-  imports: [],
+  imports: [HeavyLoadersSlowComponent, TitleComponent],
   templateUrl: './defer-views.component.html',
 })
-export default class DeferViewsComponent {
-
-}
+export default class DeferViewsComponent {}
